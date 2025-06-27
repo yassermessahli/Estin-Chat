04df2afed2e4b952af150ab7f07f683200553c5b
yassermessahli <yassermessahli0@gmail.com>
2025-06-27 14:03:13 +0000

    add chunking step

---
+++ data-pipeline/pipeline/orchestrate.py
@@ -0,0 +1,7 @@
+from ..utils.load.pdf_loader import PDFLoader
+from ..utils.split.hierarchical_splitter import HierarchicalSplitter
+from ..utils.transform.image_cleanup import ImageCleanup
+from ..utils.transform.model import Model, ModelParams
+from ..utils.transform.table_cleanup import TableCleanup
+from ..utils.transform.text_cleanup import TextCleanup
+
